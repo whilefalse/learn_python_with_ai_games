d6c5a570e8885b21007b3f36a52fb1eb3e0e7380
Steven Anderson <steve@whilefalse.net>
2012-12-05 21:00:47 +0000

    Improved my ship placement

--- battleships/players/steves_player.py
+++ battleships/players/steves_player.py
@@ -10,16 +10,16 @@ class Player(object):
         self.last_shot = None
 
         return [
-                [4, 0, 0, 'across'],
-                [3, 0, 1, 'across'],
-                [3, 1, 2, 'down'],
-                [2, 0, 2, 'down'],
-                [2, 3, 1, 'down'],
-                [2, 4, 0, 'down'],
-                [1, 2, 2, 'across'],
-                [1, 2, 3, 'across'],
-                [1, 0, 5, 'across'],
-                [1, 0, 4, 'across'],
+                [4, 6, 0, 'across'],
+                [3, 0, 1, 'down'],
+                [3, 2, 3, 'down'],
+                [2, 0, 9, 'across'],
+                [2, 6, 4, 'across'],
+                [2, 4, 6, 'down'],
+                [1, 9, 6, 'across'],
+                [1, 6, 7, 'across'],
+                [1, 7, 8, 'across'],
+                [1, 9, 9, 'across'],
                 ]
 
     def take_shot(self, board, ships):
